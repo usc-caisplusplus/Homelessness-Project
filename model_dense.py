1f8c64a39c75cdee423168702f3ac4a074fbafa5
Hayden Shively <haydenshively@gmail.com>
2019-11-09 02:16:33 -0800

    Now have equal number of positive and negative samples

--- model_dense.py
+++ model_dense.py
@@ -9,13 +9,15 @@ class Dense(Model):
     def build(self):
         inputs = layers.Input(shape = self.input_shape)
 
-        dense1 = layers.Dense(40, activation = 'relu')(inputs)
-        dense2 = layers.Dense(20, activation = 'relu')(dense1)
-        dense3 = layers.Dense(1, activation = 'sigmoid')(dense2)
+        dense1 = layers.Dense(120, activation = 'relu')(inputs)
+        dense2 = layers.Dense(60, activation = 'relu')(dense1)
+        dense3 = layers.Dense(30, activation = 'relu')(dense2)
+        dense4 = layers.Dense(15, activation = 'relu')(dense3)
+        dense5 = layers.Dense(1, activation = 'sigmoid')(dense4)
 
-        self.model = models.Model(inputs = inputs, outputs = dense3)
+        self.model = models.Model(inputs = inputs, outputs = dense5)
         super().build()
 
     def compile(self):
-        self.model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+        self.model.compile(optimizer = 'sgd', loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
         super().compile()
--- train_dense.py
+++ train_dense.py
@@ -2,6 +2,20 @@ if __name__ == '__main__':
     import numpy as np
     X = np.load('dataset/inputs.npy')
     Y = np.load('dataset/outputs.npy')
+
+    true_mask = (Y == 1).astype('bool').flatten()
+
+    X_true = X[true_mask]
+    X_false = X[~true_mask]
+    Y_true = Y[true_mask]
+    Y_false = Y[~true_mask]
+
+    smaller = min(X_true.shape[0], X_false.shape[0])
+    X = np.vstack((X_true[:smaller], X_false[:smaller]))
+    Y = np.vstack((Y_true[:smaller], Y_false[:smaller]))
+
+    print(X.shape)
+
     num_samples = X.shape[0]
     x_train = X[:(num_samples*9)//10]
     y_train = Y[:(num_samples*9)//10]
@@ -14,5 +28,7 @@ if __name__ == '__main__':
     dense.build()
     dense.compile()
 
-    dense.model.fit(x_train, y_train, epochs = 3, batch_size=64, validation_data = (x_test, y_test))
+    print(dense.model.summary())
+
+    dense.model.fit(x_train, y_train, epochs = 20, batch_size=16, validation_data = (x_test, y_test), shuffle = True)
     dense.model.save_weights('dense_weights.h5')
